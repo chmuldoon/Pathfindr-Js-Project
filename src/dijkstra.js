0a4ef8f3d3f5ee767140f25e9e89433368ca93ea
Charles Muldoon <charm3728@gmail.com>
2019-12-01 15:18:07 -0800

    fixed a* path

--- src/dijkstra.js
+++ src/dijkstra.js
@@ -1,29 +1,5 @@
 import TileNode from "./tile_node";
-//Dijkstra, bfs 
-// 1
-// Mark all nodes unvisited.Create a set of all the unvisited nodes called the unvisited set.
-// 2
-// Assign to every node a tentative distance value: set it to zero for our initial node and to infinity for all other nodes.
-//  Set the initial node as current.
-// 3
-// For the current node, consider all of its unvisited neighbours and calculate their tentative distances through the current
-// node.Compare the newly calculated tentative distance to the current assigned value and assign the smaller one.For example, 
-// if the current node A is marked with a distance of 6, and the edge connecting it with a neighbour B has length 2, then the
-// distance to B through A will be 6 + 2 = 8. If B was previously marked with a distance greater than 8 then change it to 8.
-// Otherwise, the current value will be kept.
-// 4
-// When we are done considering all of the unvisited neighbours of the current node, mark the current node as visited and
-// remove it from the unvisited set.A visited node will never be checked again.
-// 5
-// If the destination node has been marked visited(when planning a route between two specific nodes) or if the smallest
-// tentative distance among the nodes in the unvisited set is infinity(when planning a complete traversal; occurs when
-// there is no connection between the initial node and remaining unvisited nodes), then stop.The algorithm has finished.
-// 6
-// Otherwise, select the unvisited node that is marked with the smallest tentative distance, set it as the new "current node",
-//   and go back to step 3.
 
-
-/// NEW NOTE THIS ISNT ENTIRELY DIJKSTRA, so possible name change in the works 
 class Dijkstra{
   constructor(options){
     this.startPos = options.startPos;
@@ -33,6 +9,8 @@ class Dijkstra{
     this.$el = options.$el;
     this.hit = options.hit || false;
     this.diag = options.diag;
+    this.visitArr = [this.startPos];
+
   }
 
   async makePath(){
@@ -70,16 +48,22 @@ class Dijkstra{
         this.hit = true;
       }else{
         let positions = this.neighbors(currPos)
-        await this.sleep(15).then(() => {
-          $(`li[pos='${currPos[0]},${currPos[1]}']`).data("children", positions);
-        });
-     
+        
+        $(`li[pos='${currPos[0]},${currPos[1]}']`).data("children", positions);
         queue = queue.concat(positions);
       }
     }
+    for (let i = 0; i < this.visitArr.length; i++) {
+      const pos = this.visitArr[i];
+      await this.sleep(40).then(() => {
+        $(`li[pos='${pos[0]},${pos[1]}']`)
+          .data("class", "colored")
+          .addClass("colored");
+      });
+    }
     this.makePath()
   }
-  
+
   
   validMoves(pos){
     return (pos[0] >= 0 && pos[0] < this.height && pos[1] >= 0 && pos[1] < this.width)
@@ -106,6 +90,7 @@ class Dijkstra{
       const neighbor = [pos[0] + move[0], pos[1] + move[1]];
       if (!$(`li[pos='${neighbor[0]},${neighbor[1]}']`).hasClass("wall") && !$(`li[pos='${neighbor[0]},${neighbor[1]}']`).hasClass("visited") && !$(`li[pos='${neighbor[0]},${neighbor[1]}']`).hasClass("frog")&& this.validMoves(neighbor)){
         neighbors.push(neighbor)
+        this.visitArr.push(neighbor)
         $(`li[pos='${neighbor[0]},${neighbor[1]}']`)
           .data("class", "visited")
           .addClass("visited")
